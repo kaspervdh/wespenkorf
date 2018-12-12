41c810b6aafced8712a48f4790208462a3ced34a
kaspervdh <kasper.van.der.heijden10@gmail.com>
2018-12-12 23:33:46 +0100

    Created new functions
    function for random token
    function to getImage
    function to getOrdername

--- inc/functions.php
+++ inc/functions.php
@@ -12,6 +12,56 @@ function connectToDatabase()
 	return $connect;
 }
 
-?>
+function crypto_rand_secure($min, $max)
+{
+    $range = $max - $min;
+    if ($range < 1) return $min; // not so random...
+    $log = ceil(log($range, 2));
+    $bytes = (int) ($log / 8) + 1; // length in bytes
+    $bits = (int) $log + 1; // length in bits
+    $filter = (int) (1 << $bits) - 1; // set all lower bits to 1
+    do {
+        $rnd = hexdec(bin2hex(openssl_random_pseudo_bytes($bytes)));
+        $rnd = $rnd & $filter; // discard irrelevant bits
+    } while ($rnd > $range);
+    return $min + $rnd;
+}
 
+function getToken($length)
+{
+    $token = "";
+    $codeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $codeAlphabet.= "abcdefghijklmnopqrstuvwxyz";
+    $codeAlphabet.= "0123456789";
+    $max = strlen($codeAlphabet); // edited
 
+    for ($i=0; $i < $length; $i++) {
+        $token .= $codeAlphabet[crypto_rand_secure(0, $max-1)];
+    }
+
+    return $token;
+}
+
+function getOrderImage($productId){
+    $connect = connectToDatabase();  /* Starts connection to database */
+
+    $query = "SELECT `imagePath` FROM products WHERE `id` = '$productId'";
+
+    $resource = mysqli_query($connect, $query);
+
+    $image = mysqli_fetch_row($resource);
+
+    return $image[0];
+}
+
+function getOrderName($productId){
+    $connect = connectToDatabase();  /* Starts connection to database */
+
+    $query = "SELECT `name` FROM products WHERE `id` = '$productId'";
+
+    $resource = mysqli_query($connect, $query);
+
+    $name = mysqli_fetch_row($resource);
+
+    return $name[0];
+}
