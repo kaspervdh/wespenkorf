82bf94d9e4a0341fa063481b6e8a347cb2c0a853
kaspervdh <kasper.van.der.heijden10@gmail.com>
2018-12-12 21:06:10 +0100

    Update removeFromCart.php

--- inc/removeFromCart.php
+++ inc/removeFromCart.php
@@ -8,7 +8,7 @@
 
 require("initialization.php");
 
-$_SESSION["cartItemCount"] = $_SESSION["cartItemCount"] - 1;
+
 
 if(empty($_SESSION['cart'])){
     $_SESSION['cart'] = array();
@@ -17,3 +17,11 @@ else{
     $_SESSION['cart'] = array_diff($_SESSION['cart'], [$_POST['productId']]);
 }
 
+if(empty($_SESSION['cartDisplay'])){
+    $_SESSION['cartDisplay'] = array();
+}
+else{
+    $_SESSION['cartDisplay'] = array_diff($_SESSION['cartDisplay'], [$_POST['productId']]);
+}
+
+$_SESSION["cartItemCount"] = count($_SESSION["cartDisplay"]);
