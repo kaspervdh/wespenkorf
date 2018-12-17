0d65fcdadba4ae0a92a756a3b81742c6aec53607
Kasper <kasper.van.der.heijden10@gmail.com>
2018-12-17 15:05:36 +0100

    Update placeOrder.php

--- inc/placeOrder.php
+++ inc/placeOrder.php
@@ -6,7 +6,26 @@
  * Time: 3:20 PM
  */
 
-require("initialization.php");
+session_start();  /* starts session */
+require("functions.php");  /* includes all necessary functions */
+$connect = connectToDatabase();  /* Starts connection to database */
 
 echo $token = getToken(25);
+
+// count duplicates in array_count_values() function
+$quantity = array_count_values($_SESSION["cartDisplay"]);
+
+print_r($quantity);
+$orderId = 1;
+
+foreach($quantity as count($quantity)){
+    $productOrderQuery = "insert into `orderproduct` (`orderId`, `productId`, `quantity`) values (`{$orderId}`";
+    foreach($_SESSION['cartDisplay'] as $key => $productId)
+    {
+        $productOrderQuery .= "$productId";
+    }
+    $productOrderQuery .= ")";
+}
+echo $productOrderQuery;
+
 
